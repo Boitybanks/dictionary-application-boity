1da5945e08f66254a605701854ecb453201092fa
Boity <mrsboitymbhele@gmail.com>
2025-10-01 20:33:46 +0200

    added a logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import logo from "./SheCodes.png";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p> Welcome to my dictionary app! By Boity Banks </p>
+        <img src={logo} className="shecodeslogo" alt="logo" />
+        <p> Welcome to my dictionary app! By Boity </p>
       </header>
     </div>
   );
