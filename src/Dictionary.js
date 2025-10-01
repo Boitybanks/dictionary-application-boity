a2af162703b8dcdf4eeae70ff7df24c7cdb21d4b
Boity <mrsboitymbhele@gmail.com>
2025-10-02 00:09:09 +0200

    fetch data

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary(props) {
   const [keyword, setKeyword] = useState(props.defaultKeyword);
@@ -7,11 +8,16 @@ export default function Dictionary(props) {
   function handleInputChange(event) {
     setKeyword(event.target.value);
   }
-
+  function handleResponse(response) {
+    console.log(response.data);
+  }
   function handleSubmit(event) {
     event.preventDefault();
     alert(`Searching for: ${keyword}`);
     // Perform search with the keyword
+    const apiKey = "0fcte29ba005o3984f3f24530ff18441";
+    const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   return (
