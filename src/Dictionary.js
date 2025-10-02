5fed8074108f0f70e0731da83a6cf2325c48790c
Boity <mrsboitymbhele@gmail.com>
2025-10-02 14:38:11 +0200

    added Results component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import Results from "./Results";
 import axios from "axios";
 
 export default function Dictionary(props) {
   const [keyword, setKeyword] = useState(props.defaultKeyword);
+  const [definition, setDefinition] = useState("");
 
   function handleInputChange(event) {
     setKeyword(event.target.value);
   }
   function handleResponse(response) {
     console.log(response.data);
+    // better way to log the first definition:
+    setDefinition(response.data); // Use the first result if it's an array
   }
   function handleSubmit(event) {
     event.preventDefault();
@@ -17,6 +21,7 @@ export default function Dictionary(props) {
     const apiKey = "0fcte29ba005o3984f3f24530ff18441";
     const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+    console.log(apiUrl);
   }
 
   return (
@@ -27,7 +32,9 @@ export default function Dictionary(props) {
           <button type="submit">Search</button>
         </form>
         <div className="Dictionary-section">
-          <h2>{keyword}</h2>
+          <p>
+            <Results keyword={keyword} definition={definition} />
+          </p>
         </div>
       </div>
     </div>
---
+++ src/Results.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Results(props) {
+  if (props.definition && props.definition.meanings) {
+    return (
+      <div className="Results">
+        <h2>{props.definition.word}</h2>
+        <h3>Definitions:</h3>
+        {props.definition.meanings.map((meaning, index) => (
+          <div key={index}>
+            <strong>{meaning.partOfSpeech}</strong>
+            <p>{meaning.definition || "No definition found."}</p>
+            <em>{meaning.example || "No example available."}</em>
+            <hr />
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    return <p>No definition found</p>;
+  }
+}
